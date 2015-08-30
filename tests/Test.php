5c1d2348101d08e6731438215823ef2afee603b7
Robert Cooke <robert.cooke@trimm.nl>
2015-08-30 08:55:29 +0200

    cc

---
+++ tests/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace tests;
+
+class Test extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+}
