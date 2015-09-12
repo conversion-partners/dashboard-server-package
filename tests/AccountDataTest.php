95b474d5dae730530a60fdfdd8c4ce9663f2c4b3
Robert Cooke <robert.cooke@trimm.nl>
2015-09-12 10:23:32 +0200

    cc

--- tests/AccountDataTest.php
+++ tests/AccountDataTest.php
@@ -9,7 +9,7 @@ class AccountDataTest extends \PHPUnit_Framework_TestCase
     public function testAccount()
     {
         $accountPath = 'data/accounts/easydrain';
-        $server = array('REQUEST_URI' => '/contact.html','HTTP_HOST' => 'easydrain.nl');
+        $server = array('REQUEST_URI' => '/reference-hotels-w','HTTP_HOST' => 'easydrain.nl');
         $account = new AccountData($accountPath);
         $account->setServerVars($server);
         $page = $account->getPage();
