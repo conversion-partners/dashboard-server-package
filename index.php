c875f523a0df06a2d700a7434a8b3389d267400c
Robert Cooke <robert.cooke@trimm.nl>
2015-09-12 22:22:39 +0200

    cc

--- index.php
+++ index.php
@@ -2,24 +2,27 @@
 
 
 include_once dirname(__FILE__).'/src/Dashboardserver/Config.php';
-
+require __DIR__.'/vendor/autoload.php';
+use Dashboardserver\Server;
 use Dashboardserver\Config as Config;
 
 $config = new Config();
 $config->setDebug(true);
 $config->setDomains(array('easydrain.com' => 'easydrain', 'easydrain.nl' => 'easydrain'));
-$config->setOrigins(array('http://localhost:9090/' => 'active', 'http://localhost:9090/' => 'active'));
+$config->setDomainAliases(array('www.easydrain.com' => 'easydrain.com', 'acceptance.easydrain.com' => 'easydrain.com'));
+$config->setLocaleStrategy('DomainFolder');
+$config->setServerStrategy('Nginx');
+$config->setAccountPath('data/accounts/');
 
 if ($config->getDebug()) {
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
 }
 
-require __DIR__.'/vendor/autoload.php';
-use Dashboardserver\Server;
+$server_env = array(
+  'HTTP_HOST' => 'easydrain.nl',
+  'REQUEST_URI' => '/reference-hotels-w',
+  );
 
-$server = new Server();
-
-$server->setConfig($config);
-$server->setServerVars($_SERVER);
-$server->start();
+$server = new Server($config, $server_env);
+$server->init();
