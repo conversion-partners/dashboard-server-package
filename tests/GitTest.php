69453987d15a3bcf04c1185068cc2584e0aaf26d
Robert Cooke <robert.cooke@trimm.nl>
2015-09-12 16:09:05 +0200

    cc

--- tests/GitTest.php
+++ tests/GitTest.php
@@ -4,11 +4,11 @@ require __DIR__.'/../vendor/autoload.php';
 
 use SebastianBergmann\Git\Git as Git;
 
-class TestGit extends \PHPUnit_Framework_TestCase
+class GitTest extends \PHPUnit_Framework_TestCase
 {
     public function testCheckout()
     {
-        $git = new Git('/var/www/dashboard-server-package/data/testrepo/dashboard');
+        $git = new Git('/var/www/dashboard-server-package/tmp');
         $git->pull();
     }
 }
